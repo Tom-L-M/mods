487b9d71ba394a24d26dee41c2fcbe489d720a8f
no.mad <69174010+Tom-L-M@users.noreply.github.com>
2024-12-08 03:32:32 -0300

    feat(shred): Add support for removing directories and multiple targets.

--- shred/index.js
+++ shred/index.js
@@ -1,6 +1,8 @@
 const fs = require('fs');
+const path = require('path');
 const crypto = require('crypto');
 const { ArgvParser } = require('../shared');
+const WRITE_CHUNK_SIZE = 4 * 1024; // 4 Kb
 
 const help = `
     [shred-js]
@@ -9,21 +11,42 @@ const help = `
         This allows for complete file exclusion, overriding it.
 
     Usage:
-        node shred [file] [options]
+        node shred [options] [path]...
 
     Options:        
         -h | --help         Prints the help message and quits.
         -v | --version      Prints the version info and quits.
         -r | --remove       Remove the file after overriding.
         -z | --zerofill     Fills with null bytes instead of random data.
-                            This makes the process faster, but less secure.`;
+                            This makes the process faster, but less secure.
+        -a | --fail-abort   Abort the ret of the operation if it fails to
+                            open, read, find, shred, or remove a file.
+    
+    Info:
+        To batch shred items, pass in a directory path instead of a file path
+        and all items in the directory will be shredded.
+        Or, pass in multiple file paths, to remove multiple files/directories.`;
 
 function refill(buffer) {
     crypto.randomFillSync(buffer);
     return buffer;
 }
 
-const WRITE_CHUNK_SIZE = 4 * 1024; // 4 Kb
+async function readdirSyncRecursive(dirpath) {
+    return (
+        await Promise.all(
+            fs.readdirSync(dirpath).map(async file => {
+                const filepath = path.join(dirpath, file);
+                const stat = fs.statSync(filepath);
+                return stat.isDirectory()
+                    ? readdirSyncRecursive(filepath)
+                    : filepath;
+            })
+        )
+    )
+        .filter(file => file.length)
+        .flat(Infinity);
+}
 
 (async function () {
     const parser = new ArgvParser();
@@ -31,45 +54,95 @@ const WRITE_CHUNK_SIZE = 4 * 1024; // 4 Kb
     parser.option('version', { alias: 'v', allowValue: false });
     parser.option('zerofill', { alias: 'z', allowValue: false });
     parser.option('remove', { alias: 'r', allowValue: false });
-    parser.argument('file');
+    parser.option('fail-abort', { alias: 'a', allowValue: false });
     const args = parser.parseArgv();
 
     if (args.version) return console.log(require('./package.json')?.version);
-    if (args.help || !args.file) return console.log(help);
+    if (args.help || !args._.length) return console.log(help);
 
-    let remove = args.remove;
-    let zerofill = args.zerofill;
+    const remove = Boolean(args.remove);
+    const zerofill = Boolean(args.zerofill);
+    const abortOnFail = Boolean(args['fail-abort']);
 
-    let stream,
-        stats,
-        fsize,
-        written = 0;
-    try {
-        stats = fs.statSync(args.file);
-        stream = fs.createWriteStream(args.file);
-        fsize = stats.size;
-    } catch {
-        return console.log(`Error: could not find file - ${args.file}`);
+    const filelist = []; // list of file paths to shred
+    const dirlist = []; // list of dir paths to remove
+
+    for (let argfile of args._) {
+        if (!fs.existsSync(argfile)) {
+            console.log(
+                `Error: Invalid file path - Could not find "${argfile}". ` +
+                    `${abortOnFail ? 'Aborting.' : 'Skipping.'}`
+            );
+            if (abortOnFail) return;
+            else continue;
+        }
+
+        const outerstats = fs.statSync(argfile);
+        if (outerstats.isDirectory()) {
+            filelist.push(...(await readdirSyncRecursive(argfile)));
+            dirlist.push(argfile);
+        } else {
+            filelist.push(argfile);
+        }
     }
 
-    // To optimize memory usage, when there is a zerofill operation,
-    // simply create a single buffer and reuse it;
-    let sharedbuffer;
-    if (zerofill) sharedbuffer = Buffer.alloc(WRITE_CHUNK_SIZE, 0);
-    else sharedbuffer = Buffer.allocUnsafe(WRITE_CHUNK_SIZE);
+    for (let ifile of filelist) {
+        let stream,
+            stats,
+            fsize,
+            written = 0;
+        try {
+            stats = fs.statSync(ifile);
+            stream = fs.createWriteStream(ifile, { flags: 'r+' });
+            fsize = stats.size;
+        } catch {
+            console.log(
+                `Error: Invalid file path - Could not find "${ifile}". ` +
+                    `${abortOnFail ? 'Aborting.' : 'Skipping.'}`
+            );
+            if (abortOnFail) return;
+            else continue;
+        }
 
-    const writeSync = (s, d) => new Promise(r => s.write(d, r));
+        // To optimize memory usage, when there is a zerofill operation,
+        // simply create a single buffer and reuse it;
+        let sharedbuffer;
+        if (zerofill) sharedbuffer = Buffer.alloc(WRITE_CHUNK_SIZE, 0);
+        else sharedbuffer = Buffer.allocUnsafe(WRITE_CHUNK_SIZE);
 
-    for (; written + WRITE_CHUNK_SIZE < fsize; ) {
-        if (zerofill) await writeSync(stream, sharedbuffer);
-        else await writeSync(stream, refill(sharedbuffer));
-        written += WRITE_CHUNK_SIZE;
+        const writeSync = (s, d) => new Promise(r => s.write(d, r));
+
+        for (; written + WRITE_CHUNK_SIZE < fsize; ) {
+            if (zerofill) await writeSync(stream, sharedbuffer);
+            else await writeSync(stream, refill(sharedbuffer));
+            written += WRITE_CHUNK_SIZE;
+        }
+
+        if (zerofill) await writeSync(stream, Buffer.alloc(fsize - written, 0));
+        else
+            await writeSync(
+                stream,
+                refill(Buffer.allocUnsafe(fsize - written))
+            );
+
+        await new Promise(resolve => stream.close(resolve));
+
+        if (remove)
+            try {
+                fs.rmSync(ifile);
+            } catch (err) {
+                console.log(
+                    `Error: Could not remove "${ifile}" - ${err.message}. ` +
+                        `${abortOnFail ? 'Aborting.' : 'Skipping.'}`
+                );
+                if (abortOnFail) return;
+                else continue;
+            }
     }
 
-    if (zerofill) await writeSync(stream, Buffer.alloc(fsize - written, 0));
-    else await writeSync(stream, refill(Buffer.allocUnsafe(fsize - written)));
-
-    await new Promise(resolve => stream.close(resolve));
-
-    if (remove) fs.rmSync(args.file);
+    if (remove) {
+        for (let dir of dirlist) {
+            fs.rmSync(dir, { recursive: true, force: true });
+        }
+    }
 })();
