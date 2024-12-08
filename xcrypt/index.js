28fdf7f8443d2959333d148e2893b67fb0464d55
no.mad <69174010+Tom-L-M@users.noreply.github.com>
2024-12-08 02:30:41 -0300

    refactor(xcrypt): Change XOR algorithms and STDIO features.
    Add feature to read from STDIN and write to STDOUT. Implement new algorithms for XORing pairs: buffer/file, file/buffer, file/file.

--- xcrypt/index.js
+++ xcrypt/index.js
@@ -1,48 +1,261 @@
 const fs = require('node:fs');
 
-function xorFiles(file1, file2, outputFile) {
-    // Open the files
-    let fd1 = fs.openSync(file1, 'r');
-    let fd2 = fs.openSync(file2, 'r');
+const { ArgvParser, isSTDINActive, readStdinAsync } = require('../shared');
 
-    const fdOut = fs.openSync(outputFile, 'w');
+/**
+ * XOR's a file and a buffer, using the file as primary guide.
+ * Writes the result of the xor operations to the outputStream.
+ * @param {string} file A valid file path string
+ * @param {Buffer} buffer A buffer
+ * @param {WritableStream} outputStream A valid writable stream for the output
+ */
+async function xorFileAndBuffer(file, buffer, outputStream) {
+    let ftracker = 0;
+    let btracker = 0;
+    let fd = fs.openSync(file);
 
-    // Init buffers
-    const CHUNK_SIZE = Math.min(1024 * 1024, fs.fstatSync(fd2).size); // 1MB
-    const buffer1 = Buffer.alloc(CHUNK_SIZE);
-    const buffer2 = Buffer.alloc(CHUNK_SIZE);
+    const CHUNK_SIZE = Math.min(1024 * 1024, fs.fstatSync(fd).size);
+    let ouputBuffer = Buffer.alloc(CHUNK_SIZE);
+    let fbuffer = Buffer.alloc(CHUNK_SIZE);
+    let bbuffer;
+    let fbytesread;
+    let bbytesread;
 
-    // Read the first chunk of each file
-    let bytesRead1 = fs.readSync(fd1, buffer1, 0, CHUNK_SIZE, null);
-    let bytesRead2 = fs.readSync(fd2, buffer2, 0, CHUNK_SIZE, null);
+    // If there is no data in the file, return nothing (as there is nothing to encrypt)
+    if (fs.fstatSync(fd).size === 0) {
+        fs.closeSync(fd);
+        return;
+    }
 
-    // Loop until the end of the first file
-    while (bytesRead1 > 0) {
+    // If there is no data in the buffer, return the file with no modifications (as there is no key)
+    if (buffer.length === 0) {
+        return await new Promise(resolve => {
+            const stt = fs.createReadStream(file);
+            stt.on('end', () => resolve());
+            stt.pipe(outputStream);
+            fs.closeSync(fd);
+        });
+    }
+
+    // Read the first chunk of file
+    fbytesread = fs.readSync(fd, fbuffer, 0, CHUNK_SIZE, ftracker);
+    ftracker += fbytesread;
+
+    // Read the first chunk of buffer
+    bbuffer = buffer.subarray(btracker, btracker + CHUNK_SIZE);
+    bbytesread = bbuffer.length;
+    btracker += bbytesread;
+
+    while (fbytesread > 0) {
+        // If the chunk from buffer is smaller than the chunk from file
+        // Restart the buffer position tracker and concat the data
+        while (bbytesread < fbytesread) {
+            btracker = 0;
+            bbuffer = Buffer.concat([
+                bbuffer,
+                buffer.subarray(
+                    btracker,
+                    btracker + CHUNK_SIZE - bbuffer.length
+                ),
+            ]);
+            bbytesread = bbuffer.length;
+            btracker += bbytesread;
+        }
+
         // XOR the two buffers
-        for (let i = 0; i < bytesRead1; i++) {
-            buffer1[i] ^= buffer2[i];
+        for (let i = 0; i < fbytesread; i++) {
+            ouputBuffer[i] = fbuffer[i] ^ bbuffer[i];
         }
 
         // Write the result to the output file
-        fs.writeSync(fdOut, buffer1, 0, bytesRead1);
+        await new Promise(resolve => {
+            outputStream.write(ouputBuffer, () => {
+                ouputBuffer.fill(0);
+                resolve();
+            });
+        });
 
         // Read the next chunk of the first file
-        bytesRead1 = fs.readSync(fd1, buffer1, 0, CHUNK_SIZE, null);
-
-        // If the second file has ended, reset its position to the beginning
-        if (bytesRead2 < CHUNK_SIZE) {
-            fs.closeSync(fd2);
-            fd2 = fs.openSync(file2, 'r');
-        }
-
-        // Read the next chunk of the second file
-        bytesRead2 = fs.readSync(fd2, buffer2, 0, CHUNK_SIZE, null);
+        fbytesread = fs.readSync(fd, fbuffer, 0, CHUNK_SIZE, ftracker);
+        ftracker += fbytesread;
+        // Read the next chunk of buffer
+        bbuffer = buffer.subarray(btracker, btracker + CHUNK_SIZE);
+        bbytesread = bbuffer.length;
+        btracker += bbytesread;
     }
 
-    // Close the files
+    fs.closeSync(fd);
+}
+
+/**
+ * XOR's a buffer and a file, using the buffer as primary guide.
+ * Writes the result of the xor operations to the outputStream.
+ * @param {Buffer} buffer A buffer
+ * @param {string} file A valid file path string
+ * @param {WritableStream} outputStream A valid writable stream for the output
+ */
+async function xorBufferAndFile(buffer, file, outputStream) {
+    let ftracker = 0;
+    let btracker = 0;
+    let fd = fs.openSync(file);
+
+    const CHUNK_SIZE = Math.min(1024 * 1024, buffer.length);
+    let tmpBuffer = Buffer.alloc(CHUNK_SIZE);
+    let ouputBuffer = Buffer.alloc(CHUNK_SIZE);
+    let fbuffer = Buffer.alloc(CHUNK_SIZE);
+    let bbuffer;
+    let fbytesread;
+    let bbytesread;
+
+    // If there is no data in the buffer, return nothing (as there is nothing to encrypt)
+    if (buffer.length === 0) {
+        fs.closeSync(fd);
+        return;
+    }
+    // If there is no data in the file, return the buffer with no modifications (as there is no key)
+    if (fs.fstatSync(fd).size === 0) {
+        return await new Promise(resolve => {
+            fs.closeSync(fd);
+            outputStream.write(buffer, () => resolve);
+        });
+    }
+
+    // Read the first chunk of buffer
+    bbuffer = buffer.subarray(btracker, btracker + CHUNK_SIZE);
+    bbytesread = bbuffer.length;
+    btracker += bbytesread;
+
+    // Read the first chunk of file
+    fbytesread = fs.readSync(fd, fbuffer, 0, CHUNK_SIZE, ftracker);
+    ftracker += fbytesread;
+
+    while (bbytesread > 0) {
+        // If the chunk from file is smaller than the chunk from buffer
+        // Restart the file position tracker and concat the data
+        while (fbytesread < bbytesread) {
+            fbuffer = fbuffer.subarray(0, fbytesread);
+            ftracker = 0;
+            tmpBuffer = tmpBuffer.fill(0);
+            fbytesread = fs.readSync(fd, tmpBuffer, 0, CHUNK_SIZE, ftracker);
+            ftracker += fbytesread;
+            fbuffer = Buffer.concat([
+                fbuffer,
+                tmpBuffer.subarray(0, fbytesread),
+            ]);
+            fbytesread = fbuffer.length;
+        }
+
+        // XOR the two buffers
+        for (let i = 0; i < bbytesread; i++) {
+            ouputBuffer[i] = bbuffer[i] ^ fbuffer[i];
+        }
+
+        // Write the result to the output file
+        await new Promise(resolve => {
+            outputStream.write(ouputBuffer, () => {
+                ouputBuffer.fill(0);
+                resolve();
+            });
+        });
+
+        // Read the next chunk of buffer
+        bbuffer = buffer.subarray(btracker, btracker + CHUNK_SIZE);
+        bbytesread = bbuffer.length;
+        btracker += bbytesread;
+        // Read the next chunk of the first file
+        fbytesread = fs.readSync(fd, fbuffer, 0, CHUNK_SIZE, ftracker);
+        ftracker += fbytesread;
+    }
+
+    fs.closeSync(fd);
+}
+
+/**
+ * XOR's a file1 and a file2, using the file1 as primary guide.
+ * Writes the result of the xor operations to the outputStream.
+ * @param {string} file1 A valid file path string
+ * @param {string} file2 A valid file path string
+ * @param {WritableStream} outputStream A valid writable stream for the output
+ */
+async function xorFiles(file1, file2, outputStream) {
+    let tracker1 = 0;
+    let tracker2 = 0;
+    let fd1 = fs.openSync(file1);
+    let fd2 = fs.openSync(file2);
+
+    const CHUNK_SIZE = Math.min(1024 * 1024, fs.fstatSync(fd1).size);
+    let tmpBuffer = Buffer.alloc(CHUNK_SIZE);
+    let ouputBuffer = Buffer.alloc(CHUNK_SIZE);
+    let buffer1 = Buffer.alloc(CHUNK_SIZE);
+    let buffer2 = Buffer.alloc(CHUNK_SIZE);
+    let bytesread1;
+    let bytesread2;
+
+    // If there is no data in the file1, return nothing (as there is nothing to encrypt)
+    if (fs.fstatSync(fd1).size === 0) {
+        fs.closeSync(fd1);
+        fs.closeSync(fd2);
+        return;
+    }
+
+    // If there is no data in the file2, return the file1 with no modifications (as there is no key)
+    if (fs.fstatSync(fd2).size === 0) {
+        return await new Promise(resolve => {
+            const stt = fs.createReadStream(file1);
+            stt.on('end', () => resolve());
+            stt.pipe(outputStream);
+            fs.closeSync(fd1);
+            fs.closeSync(fd2);
+        });
+    }
+
+    // Read the first chunk of file1
+    bytesread1 = fs.readSync(fd1, buffer1, 0, CHUNK_SIZE, tracker1);
+    tracker1 += bytesread1;
+
+    // Read the first chunk of file2
+    bytesread2 = fs.readSync(fd2, buffer2, 0, CHUNK_SIZE, tracker2);
+    tracker2 += bytesread2;
+
+    while (bytesread1 > 0) {
+        // If the chunk from buffer is smaller than the chunk from file
+        // Restart the buffer position tracker and concat the data
+        while (bytesread2 < bytesread1) {
+            buffer2 = buffer2.subarray(0, bytesread2);
+            tracker2 = 0;
+            tmpBuffer = tmpBuffer.fill(0);
+            bytesread2 = fs.readSync(fd2, tmpBuffer, 0, CHUNK_SIZE, tracker2);
+            tracker2 += bytesread2;
+            buffer2 = Buffer.concat([
+                buffer2,
+                tmpBuffer.subarray(0, bytesread2),
+            ]);
+            bytesread2 = buffer2.length;
+        }
+
+        // XOR the two buffers
+        for (let i = 0; i < bytesread1; i++) {
+            ouputBuffer[i] = buffer1[i] ^ buffer2[i];
+        }
+
+        // Write the result to the output file
+        await new Promise(resolve => {
+            outputStream.write(ouputBuffer, () => {
+                ouputBuffer.fill(0);
+                resolve();
+            });
+        });
+
+        // Read the next chunk of the first file
+        bytesread1 = fs.readSync(fd1, buffer1, 0, CHUNK_SIZE, tracker1);
+        tracker1 += bytesread1;
+        // Read the first chunk of file2
+        bytesread2 = fs.readSync(fd2, buffer2, 0, CHUNK_SIZE, tracker2);
+        tracker2 += bytesread2;
+    }
+
     fs.closeSync(fd1);
     fs.closeSync(fd2);
-    fs.closeSync(fdOut);
 }
 
 (async function main() {
@@ -51,55 +264,94 @@ function xorFiles(file1, file2, outputFile) {
         A tool for quickly encrypting files with the classic one-time-pad XOR cypher.
 
     Usage:
-        xcrypt [options] <data-file> <key-file> [out-file]
+        xcrypt [options] <file> <key>
+       OR
+        <stdin> | xcrypt [options] <key>      # Read the file to encrypt from STDIN 
+       OR
+        <stdin> | xcrypt [options] -s <file>  # Read the encryption key from STDIN
 
     Options:
         -h | --help         Prints the help message and quits.
         -v | --version      Prints the version info and quits.
+        -s | --stdin-key    Read <key> instead of <file> from STDIN.
+                            Has no effect if no data is passed in STDIN.
+        -o | --output FILE  The name of the file to output the content to.
+                            If one is not specified, content is sent to STDOUT.`;
 
-    Info:
-        > Encrypt/decrypt 'data-file' with data from 'key-file' as key, outputting content to 'out-file'.
-        > If 'output-file' is not provided, it defaults to 'data-file' with 'lock' extension appended.
-        > If 'output-file' is not provided, and target file already includes a 'lock' extension, it is replaced by 'bin'.
+    const parser = new ArgvParser();
+    parser.option('help', { alias: 'h', allowValue: false });
+    parser.option('version', { alias: 'v', allowValue: false });
+    parser.option('stdin-key', { alias: 's', allowValue: false });
+    parser.option('output', { alias: 'o' });
+    const args = parser.parseArgv();
 
-    Theory:
-        One-time-pad cyphers, such as XOR, have a perfect-secrecy factor, meaning that 
-        if the key size is at least the same size as the data to encrypt, it is virtually 
-        impossible to crack it, even with a theorically-infinite computational power.`;
+    if (args.version) return console.log(require('./package.json')?.version);
+    if (args.help || args._.length === 0) return console.log(help);
 
-    const args = process.argv.slice(2);
-    const arg_file = args[0];
-    const arg_skey = args[1];
-    const arg_wout = args[2] || arg_file + '.lock';
+    const fromSTDIN = isSTDINActive();
+    const stdin = fromSTDIN ? await readStdinAsync() : null;
 
-    if (args.includes('-v') || args.includes('--version'))
-        return console.log(require('./package.json')?.version);
-    if (['-h', '--help', '', undefined].includes(arg_file))
-        return console.log(help);
-    if (args.includes('-h') || args.includes('--help'))
-        return console.log(help);
+    // Validate provided parameter <file>
+    const file = stdin && !args['stdin-key'] ? stdin : args._[0];
+    const fileFromCli = file == args._[0];
+    if (!file) {
+        return console.log('Error: Missing <file> parameter');
+    }
+    if (fileFromCli) {
+        if (!fs.existsSync(file)) {
+            return console.log(
+                `Error: Invalid <file> parameter - Could not locate "${file}"`
+            );
+        }
+        const filestat = fs.statSync(file);
+        if (!filestat.isFile()) {
+            return console.log(
+                `Error: Invalid <file> parameter - "${file}" is not a file`
+            );
+        }
+    }
 
-    if (args.length < 2 || args.length > 3)
-        return console.log(
-            `<> Error: Missing parameters - expected [2 or 3], got [${args.length}]`
-        );
+    // Validate provided parameter <key>
+    const key =
+        stdin && args['stdin-key'] ? stdin : stdin ? args._[0] : args._[1];
+    const keyFromCli = key == args._[0] || key == args._[1];
+    if (!key) {
+        return console.log('Error: Missing <key> parameter');
+    }
+    if (keyFromCli) {
+        if (!fs.existsSync(key)) {
+            return console.log(
+                `Error: Invalid <key> parameter - Could not locate "${key}"`
+            );
+        }
+        const keystat = fs.statSync(key);
+        if (!keystat.isFile()) {
+            return console.log(
+                `Error: Invalid <key> parameter - "${key}" is not a file`
+            );
+        }
+    }
 
-    if (!fs.existsSync(arg_file))
-        return console.log(
-            `<> Error: Invalid file passed as parameter [${arg_file}]`
-        );
-    if (!fs.existsSync(arg_skey))
-        return console.log(
-            `<> Error: Invalid file passed as parameter [${arg_skey}]`
-        );
+    const outfile = args.output || null;
+    if (outfile && fs.existsSync(args.output)) {
+        if (!fs.statSync(args.output).isFile()) {
+            return console.log(
+                `Error: Cannot write to non-file entity "${args.output}"`
+            );
+        }
+    }
 
-    const stat1 = fs.statSync(arg_file).size;
-    const stat2 = fs.statSync(arg_skey).size;
+    const outputstream = outfile
+        ? fs.createWriteStream(outfile)
+        : process.stdout;
 
-    if (stat1 > stat2)
-        console.log(
-            `<> Warning: The provided key file [${stat2} bytes] is smaller than the data file [${stat1} bytes] - This weakens the security level considerably`
-        );
+    if (Buffer.isBuffer(file)) {
+        return await xorBufferAndFile(file, key, outputstream);
+    }
 
-    xorFiles(arg_file, arg_skey, arg_wout);
+    if (Buffer.isBuffer(key)) {
+        return xorFileAndBuffer(file, key, outputstream);
+    }
+
+    return await xorFiles(file, key, outputstream);
 })();
